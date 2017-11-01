ab8821f2ea4f0d50c7bf4ef289dc2b690227ed2b
Manuel <manu.asi2@gmail.com>
2017-11-01 06:09:31 +0100

    extracting geolocated #metoo hashtags

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ let contador=0;
 mongoose.Promise = global.Promise
 
 const T = new Twit({
-  consumer_key:         'Ts747aQy5TLEfePmFbCvecOmQ',
-  consumer_secret:      'uNOymY7FyFfD4nIONm14miDunqziiiHXjdDvviWWBAt2gHI6G3',
-  access_token:         '3060608854-q5tnCNnwzf3Y333pEM3FAjzAv0cSqt59nL35z06',
-  access_token_secret:  'XT8tphBHpRUOTzHFGXf8GTn6VuDQ0OU0npIVg6eDMH29x',
+  consumer_key:         env.process.consumer_ket,
+  consumer_secret:      env.process.consumer_secret,
+  access_token:         env.process.access_token,
+  access_token_secret:  env.process.access_token_secret,
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 });
 
